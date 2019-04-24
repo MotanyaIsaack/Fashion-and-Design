ff95b9b40e910bd29ec67433bb8a2f6d89371f5a
Rosanne Odiero <rosanneodiero9@gmail.com>
2019-04-24 04:00:59 +0300

    Our Story

--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -5,11 +5,31 @@ class Admin extends MY_Controller {
 
   	public function index()
 	{
-		
+		$this->load->view('head');
 		$this->load->view('login');
+		
 	}
 	public function signup()
 	{
+		$this->load->view('head');
 		$this->load->view('signup');
+	}
+	public function profile()
+	{
+		$this->load->view('head');
+		$this->load->view('navigation');
+		$this->load->view('header');
+		$this->load->view('profile');
+		$this->load->view('footer');
+		
+	}
+	public function ourstory()
+	{
+		$this->load->view('head');
+		$this->load->view('navigation');
+		$this->load->view('header');
+		$this->load->view('ourstory');
+		$this->load->view('footer');
+
 	}
 }
---
+++ application/modules/admin/views/navigation.php
@@ -0,0 +1,175 @@
+ <div id="page-container" class="sidebar-o sidebar-inverse enable-page-overlay side-scroll page-header-fixed main-content-narrow">
+            <!-- Side Overlay-->
+            <aside id="side-overlay">
+                <!-- Side Header -->
+                <div class="content-header content-header-fullrow">
+                    <div class="content-header-section align-parent">
+                        <!-- Close Side Overlay -->
+                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                        <button type="button" class="btn btn-circle btn-dual-secondary align-v-r" data-toggle="layout" data-action="side_overlay_close">
+                            <i class="fa fa-times text-danger"></i>
+                        </button>
+                        <!-- END Close Side Overlay -->
+
+                        <!-- User Info -->
+                        <div class="content-header-item">
+                            <a class="img-link mr-5" href="be_pages_generic_profile.html">
+                                <img class="img-avatar img-avatar32" src="<?=base_url();?>assets/admin/assets/media/avatars/avatar15.jpg" alt="">
+                            </a>
+                            <a class="align-middle link-effect text-primary-dark font-w600" href="be_pages_generic_profile.html">John Smith</a>
+                        </div>
+                        <!-- END User Info -->
+                    </div>
+                </div>
+                <!-- END Side Header -->
+
+                <!-- Side Content -->
+                <div class="content-side">
+                    <!-- Search -->
+                    <div class="block pull-t pull-r-l">
+                        <div class="block-content block-content-full block-content-sm bg-body-light">
+                            <form action="be_pages_generic_search.html" method="post">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="side-overlay-search" name="side-overlay-search" placeholder="Search..">
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-secondary px-10">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- END Search -->
+
+                   
+                  
+
+                    
+
+                    
+
+                   
+                <!-- END Side Content -->
+            </aside>
+            <!-- END Side Overlay -->
+
+            <!-- Sidebar -->
+            <!--
+                Helper classes
+
+                Adding .sidebar-mini-hide to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
+                Adding .sidebar-mini-show to an element will make it visible (opacity: 1) when the sidebar is in mini mode
+                    If you would like to disable the transition, just add the .sidebar-mini-notrans along with one of the previous 2 classes
+
+                Adding .sidebar-mini-hidden to an element will hide it when the sidebar is in mini mode
+                Adding .sidebar-mini-visible to an element will show it only when the sidebar is in mini mode
+                    - use .sidebar-mini-visible-b if you would like to be a block when visible (display: block)
+            -->
+            <nav id="sidebar">
+                <!-- Sidebar Content -->
+                <div class="sidebar-content">
+                    <!-- Side Header -->
+                    <div class="content-header content-header-fullrow px-15">
+                        <!-- Mini Mode -->
+                        <div class="content-header-section sidebar-mini-visible-b">
+                            <!-- Logo -->
+                            <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
+                                <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                            </span>
+                            <!-- END Logo -->
+                        </div>
+                        <!-- END Mini Mode -->
+
+                        <!-- Normal Mode -->
+                        <div class="content-header-section text-center align-parent sidebar-mini-hidden">
+                            <!-- Close Sidebar, Visible only on mobile screens -->
+                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                            <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
+                                <i class="fa fa-times text-danger"></i>
+                            </button>
+                            <!-- END Close Sidebar -->
+
+                            <!-- Logo -->
+                            <div class="content-header-item">
+
+                                <a  href="index.html">
+
+                                    <img src="<?=base_url();?>assets/admin/assets/media/photos/logokikoromeo1.png" width="50" >
+                                    
+                                </a>
+                            </div>
+                            <!-- END Logo -->
+                        </div>
+                        <!-- END Normal Mode -->
+                    </div>
+                    <!-- END Side Header -->
+
+                    <!-- Side User -->
+                    <div class="content-side content-side-full content-side-user px-10 align-parent">
+                        <!-- Visible only in mini mode -->
+                        <div class="sidebar-mini-visible-b align-v animated fadeIn">
+                            <img class="img-avatar img-avatar32" src="<?=base_url();?>assets/admin/assets/media/avatars/avatar15.jpg" alt="">
+                        </div>
+                        <!-- END Visible only in mini mode -->
+
+                        <!-- Visible only in normal mode -->
+                        <div class="sidebar-mini-hidden-b text-center">
+                            <a class="img-link" href="be_pages_generic_profile.html">
+                                <img class="img-avatar" src="<?=base_url();?>assets/admin/assets/media/avatars/avatar15.jpg" alt="">
+                            </a>
+                            <ul class="list-inline mt-10">
+                                <li class="list-inline-item">
+                                    <a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">R. Odiero</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                                    <a class="link-effect text-dual-primary-dark" data-toggle="layout" data-action="sidebar_style_inverse_toggle" href="javascript:void(0)">
+                                        <i class="si si-drop"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="link-effect text-dual-primary-dark" href="op_auth_signin.html">
+                                        <i class="si si-logout"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- END Visible only in normal mode -->
+                    </div>
+                    <!-- END Side User -->
+
+                    <!-- Side Navigation -->
+                    <div class="content-side content-side-full">
+                        <ul class="nav-main">
+                            <li>
+                                <a href="<?=base_url();?>admin/ourstory"><i class="si si-cup"></i><span class="sidebar-mini-hide">Our Story</span></a>
+                            </li>
+                            <li class="open">
+                                <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Collections</span></a>
+                                <ul>
+                                    <li class="open">
+                                        <a  href="#"><span class="sidebar-mini-hide">Men</span></a>
+                                        
+                                            <li>
+                                                <a class="active" href="be_pages_dashboard2.html"><span class="sidebar-mini-hide">Women</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_dashboard3.html"><span class="sidebar-mini-hide">Vintage</span></a>
+                                            </li>
+                                            
+                                        
+                                    </li>
+                                </ul>
+                                    <li>
+                                <a class="nav-submenu" data-toggle="nav-submenu" href="be_pages_dashboard.html"><span class="sidebar-mini-hide">Events</span></a>
+                            </li>
+                                   
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END Side Navigation -->
+                </div>
+                <!-- Sidebar Content -->
+            </nav>
+            <!-- END Sidebar -->
